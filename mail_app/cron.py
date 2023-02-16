7e6286df168ef2e14f09dea63a6d84f0b3c97f4b
anna-golovacheva <anna.ch.ollala@gmail.com>
2023-02-16 15:00:02 +0300

    time was fixed

--- mail_app/cron.py
+++ mail_app/cron.py
@@ -1,10 +1,7 @@
-from django.utils import timezone
-
-from config.settings import EMAIL_HOST_USER
-from mail_app.management.commands.start_mailing import Command
+from config.settings import TIME_ZONE
+from mail_app.management.commands.start_mailing import do_mailing
 from mail_app.models import Mailing, MailingAttempt
 from datetime import datetime, timedelta
-from django.core.mail import send_mail
 
 
 def my_scheduled_job():
@@ -12,18 +9,17 @@ def my_scheduled_job():
     to_send = False
     now = datetime.now()
     mailings = Mailing.objects.filter(status__in=[Mailing.STARTED, Mailing.CREATED])
+
     for mailing in mailings:
         if mailing.time.strftime("%H:%M") == now.strftime("%H:%M"):
             last_attempt = MailingAttempt.objects.filter(mailing=mailing.id).last()
+
             if not last_attempt:
                 to_send = True
             else:
                 from_last = now.date() - last_attempt.time.date()
                 if mailing.frequency == Mailing.MONTHLY and from_last == timedelta(days=30) or mailing.frequency == Mailing.WEEKLY and from_last == timedelta(days=7) or mailing.frequency == Mailing.DAILY and from_last == timedelta(days=1):
                     to_send = True
+
         if to_send:
-            c = Command
-            c.handle(mailing.id)
-
-
-
+            do_mailing(mailing, mailing.id)
--- mail_app/management/commands/start_mailing.py
+++ mail_app/management/commands/start_mailing.py
@@ -1,9 +1,53 @@
 from django.core.mail import send_mail
 from django.core.management import BaseCommand
-from django.core import management
 
 from config.settings import EMAIL_HOST_USER
 from mail_app.models import Mailing, MailingAttempt
+
+
+def do_mailing(mailing, pk):
+    mailing.status = Mailing.STARTED
+    mailing.save()
+
+    print(f'Mailing {pk} has started.')
+
+    try:
+        recipients = mailing.recipient.all()
+        rec_list = [rec.email for rec in recipients]
+        result = send_mail(
+            subject=mailing.message.topic,
+            message=mailing.message.body,
+            from_email=EMAIL_HOST_USER,
+            recipient_list=rec_list,
+            fail_silently=False
+        )
+
+        if result:
+            mailing_attempt = MailingAttempt.objects.create(
+                status=MailingAttempt.SUCCESS,
+                answer=200,
+                mailing_id=mailing.id
+            )
+
+            print(f'Mailing has been sent. Success mailing attempt {mailing_attempt.id} is created.')
+
+        else:
+            mailing_attempt = MailingAttempt.objects.create(
+                status=MailingAttempt.FAIL,
+                answer='Сообщение не отправлено (получателя нет либо указан неверный адрес почты)',
+                mailing_id=mailing.id
+            )
+
+            print(f'Mailing has not been sent. Fail mailing attempt {mailing_attempt.id} is created.')
+
+    except Exception as err:
+        mailing_attempt = MailingAttempt.objects.create(
+            status=MailingAttempt.FAIL,
+            answer=err,
+            mailing_id=mailing.id
+        )
+
+        print(f'Mailing has not been sent. Fail mailing attempt {mailing_attempt.id} is created.')
 
 
 class Command(BaseCommand):
@@ -12,41 +56,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         pk = args[0]
-        mailing = Mailing.objects.filter(pk=pk).first()
-        mailing.status = Mailing.STARTED
-        mailing.save()
-        print(f'Mailing {pk} has started.')
-
-        try:
-            recipients = mailing.recipient.all()
-            rec_list = [rec.email for rec in recipients]
-            result = send_mail(
-                subject=mailing.message.topic,
-                message=mailing.message.body,
-                from_email=EMAIL_HOST_USER,
-                recipient_list=rec_list,
-                fail_silently=False
-            )
-
-            if result:
-                mailing_attempt = MailingAttempt.objects.create(
-                    status=MailingAttempt.SUCCESS,
-                    answer=200,
-                    mailing_id=mailing.id
-                )
-                print(f'Mailing has been sent. Success mailing attempt {mailing_attempt.id} is created.')
-
-            else:
-                mailing_attempt = MailingAttempt.objects.create(
-                    status=MailingAttempt.FAIL,
-                    answer='Сообщение не отправлено (получателя нет либо указан неверный адрес почты)',
-                    mailing_id=mailing.id
-                )
-                print(f'Mailing has not been sent. Fail mailing attempt {mailing_attempt.id} is created.')
-        except Exception as err:
-            mailing_attempt = MailingAttempt.objects.create(
-                status=MailingAttempt.FAIL,
-                answer=err,
-                mailing_id=mailing.id
-            )
-            print(f'Mailing has not been sent. Fail mailing attempt {mailing_attempt.id} is created.')
+        mlng = Mailing.objects.filter(pk=pk).first()
+        do_mailing(mlng, pk)
